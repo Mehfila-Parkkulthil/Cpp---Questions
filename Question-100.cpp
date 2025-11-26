fe1a91f6ebaedabf01512905fb5452ca529a677d
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-27 00:00:51 +0530

    modified

--- Question-100.cpp
+++ Question-100.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
 
   for(int i=1;i<=100;i++){
-    // cout << "| %d| |[Click Here](https://github.com/Mehfila-Parkkulthil/Cpp---Questions/blob/main/Question-%d.cpp) | ", i ;
-    cout << "%d Hello ",i;
+     cout << "|" << i << "|" << "|[Click Here](https://github.com/Mehfila-Parkkulthil/Cpp---Questions/blob/main/Question-"<<i<<".cpp"<< " | " << "\n";
+   
   }
 
 }
