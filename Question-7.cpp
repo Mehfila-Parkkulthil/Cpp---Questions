36af804f3b9aa023139d572e1a266e6a6a18259b
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-12-11 20:47:09 +0530

    pushed 6,7

--- Question-7.cpp
+++ Question-7.cpp
@@ -3,8 +3,13 @@ Print factorial of n
 */
 
 #include <iostream>
+using namespace std;
 int main(){
-
-
+ int n =5;
+ int prod =1;  
+ for(int i=1;i<=n;i++){
+  prod*=i;
+ }
+cout <<prod;
   return 0;
 }
