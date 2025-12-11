93fd66450e84b9f2dac622886103a0ee3a87dbc1
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-12-12 01:29:27 +0530

    pushed 6,7

--- Question-8.cpp
+++ Question-8.cpp
@@ -1,8 +1,24 @@
-/*Question 
+/*Question 8
+Create this pattern
+A
+AB
+ABC
+ABCD
 */
 
 #include <iostream>
+using namespace std;
 int main(){
+  int n=5;
+
+  for(int i=0;i<n;i++){
+      char ch ='A';
+    for(int j=0;j<i+1;j++){
+      cout<< ch;
+      ch=ch+1;
+    }
+    cout<<endl;
+  }
 
 
   return 0;
