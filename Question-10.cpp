55d177526386d7fe412812d86d717ca700b5d020
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-01-09 13:54:50 +0530

    modified FQuestion10

--- Question-10.cpp
+++ Question-10.cpp
@@ -1,9 +1,17 @@
-/*Question 
+/* Question 10
+Bitwise Operator
 */
 
 #include <iostream>
+using namespace std;
 int main(){
-
-
-  return 0;
-}
+  cout<<"Hello"<< endl;
+  int a,b;
+  a =6 , b=10;
+  //Bitwise operators
+  cout<< (a & b) << endl;
+  cout<< (a | b)<< endl;
+  cout<< (a ^ b) << endl;
+  cout<< (b << 2) << endl;
+  cout<< (b >> 1)<< endl;
+ }
